4b758f8ea8ee00cdbb2555f2b0e89528bcc22f87
bokostmu <32546026+bokostmu@users.noreply.github.com>
2017-12-27 21:29:31 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+/**
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills
+ * nodejs skill development kit.
+ * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
+ * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
+ * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
+ **/
+
+'use strict';
+
+const Alexa = require('alexa-sdk');
+
+const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+
+const languageStrings = {
+    'de-DE': {
+        'translation': {
+            'WELCOME_OUTPUT' : 'Herzlich Willkommen, was möchtest du testen?'
+        }
+    }
+};
+
+const handlers = {
+    'LaunchRequest': function () {
+        this.response.speak(this.t('WELCOME_OUTPUT')).listen(this.t('WELCOME_OUTPUT'));
+        this.emit(':responseReady');
+    },
+    'TestIntent': function () {
+        this.response.speak("Alles klar! Erfolgreich getestet.");
+        this.emit(':responseReady');
+    },
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = this.t('HELP_MESSAGE');
+        const reprompt = this.t('HELP_MESSAGE');
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+    'Unhandled': function () {
+        console.log('UNHANDLED ERROR:' + JSON.stringify(this.event.request))
+    }
+};
+
+exports.handler = function (event, context) {
+    const alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
